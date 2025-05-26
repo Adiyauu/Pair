dcf8d2e1d3d0d033cbfd0973b617b12c95ac4a46
RAVINDU_MD <Katharinabto@gmail.com>
2025-05-26 17:54:17 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "𝙼𝙰𝙳𝙰𝚄𝚂𝙰𝙽𝙺𝙰-𝙼𝙳=" + string_session;
+                        let md = "𝙼𝙰𝙳𝚄𝚂𝙰𝙽𝙺𝙰-𝙼𝙳=" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*𝙳𝚘𝚗𝚝 𝚜𝚑𝚊𝚛𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚠𝚒𝚝𝚑 𝚊𝚗𝚢𝚘𝚗𝚎!! 𝚄𝚜𝚎 𝚝𝚑𝚒𝚜 𝚌𝚘𝚍𝚎 𝚝𝚘 𝚌𝚛𝚎𝚊𝚝𝚎 MADUSANKA MD 𝚆𝚑𝚊𝚝𝚜𝚊𝚙𝚙 𝚄𝚜𝚎𝚛 𝚋𝚘𝚝.*\n\n ◦ *Github:* https://github.com/Adiyauu/rmadusanka_Md`; 
                         await sock.sendMessage(sock.user.id, {
